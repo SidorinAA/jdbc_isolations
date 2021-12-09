a22350f4ff1272a08a06f35322171f9c95a31110
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-12-09 23:21:56 +0300

    test3

--- src/main/java/base/MainServlet.java
+++ src/main/java/base/MainServlet.java
@@ -6,13 +6,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
 @WebServlet(urlPatterns = {"/cool"})
 public class MainServlet extends HttpServlet {
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello world!!");
+        PrintWriter pw = resp.getWriter();
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/java_isolation", "postgres", "postgres");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select name from users");
+
+            while(rs.next()){
+                pw.print(rs.getString("name"));
+            }
+
+
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 }
