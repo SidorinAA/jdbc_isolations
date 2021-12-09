94c9ae53837d8b5b15a2975e51b62e456b87c3cc
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-12-09 23:02:36 +0300

    test2

--- src/main/java/base/MainServlet.java
+++ src/main/java/base/MainServlet.java
@@ -13,6 +13,6 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello world!");
+        resp.getWriter().write("Hello world!!");
     }
 }
